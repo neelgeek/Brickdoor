8aff422cb19022774dfba726f23ddae955db8376
neelgeek <neelbhave91@gmail.com>
2020-04-18 21:17:39 -0400

    Pulling api

--- brickdoor/src/main/java/com/example/brickdoor/controllers/ReviewController.java
+++ brickdoor/src/main/java/com/example/brickdoor/controllers/ReviewController.java
@@ -11,14 +11,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-import javax.persistence.Column;
 import javax.servlet.http.HttpSession;
 
 @RestController
--- brickdoor/src/main/java/com/example/brickdoor/controllers/UserController.java
+++ brickdoor/src/main/java/com/example/brickdoor/controllers/UserController.java
@@ -3,22 +3,18 @@ package com.example.brickdoor.controllers;
 import com.example.brickdoor.daos.UserDao;
 import com.example.brickdoor.models.Admin;
 import com.example.brickdoor.models.Company;
-
 import com.example.brickdoor.models.Role;
 import com.example.brickdoor.models.Student;
 import com.example.brickdoor.models.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.ui.Model;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
 
