44c9cb38eb41782f9236221c91ea0f2fd35d4b83
Omar Tuffaha <tuffaha.o@husky.neu.edu>
2020-04-19 23:00:46 -0400

    new apis

--- brickdoor/src/main/java/com/example/brickdoor/repositories/AdminRepository.java
+++ brickdoor/src/main/java/com/example/brickdoor/repositories/AdminRepository.java
@@ -1,6 +1,9 @@
 package com.example.brickdoor.repositories;
 
 import com.example.brickdoor.models.Admin;
+import com.example.brickdoor.models.Role;
+import java.util.List;
+import java.util.Set;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -11,6 +14,17 @@ public interface AdminRepository extends CrudRepository<Admin,Integer> {
 
   @Query("SELECT admin FROM User admin WHERE admin.id=:adminId")
   Admin findAdminById(@Param("adminId") int adminId);
+
+  @Query("SELECT admin FROM User admin WHERE admin.role=:role")
+  List<Admin> getAllAdmin(@Param("role") Role role);
+
+  @Query(
+      "SELECT admins FROM User admins WHERE admins.role=:role AND "
+          + "(admins.username LIKE :query% "
+          + "OR admins.firstName LIKE :query% "
+          + "OR admins.lastName LIKE :query%)"
+  )
+  Set<Admin> searchAdmin(@Param("query") String query, @Param("role") Role role);
 
 }
 
--- brickdoor/src/main/java/com/example/brickdoor/repositories/StudentRepository.java
+++ brickdoor/src/main/java/com/example/brickdoor/repositories/StudentRepository.java
@@ -1,6 +1,9 @@
 package com.example.brickdoor.repositories;
 
+import com.example.brickdoor.models.Role;
 import com.example.brickdoor.models.Student;
+import java.util.List;
+import java.util.Set;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,4 +13,15 @@ import org.springframework.stereotype.Repository;
 public interface StudentRepository extends CrudRepository<Student,Integer> {
   @Query("SELECT student FROM User student WHERE student.id=:studentId")
   Student findStudentById(@Param("studentId") int studentId);
+
+  @Query("SELECT student FROM User student WHERE student.role=:role")
+  List<Student> getAllStudents(@Param("role") Role role);
+
+  @Query(
+      "SELECT students FROM User students WHERE students.role=:role AND "
+          + "(students.username LIKE :query% "
+          + "OR students.firstName LIKE :query% "
+          + "OR students.lastName LIKE :query%)"
+  )
+  Set<Student> searchStudents(@Param("query") String query, @Param("role") Role role);
 }
