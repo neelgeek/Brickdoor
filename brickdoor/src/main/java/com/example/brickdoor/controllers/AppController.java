afab78d3f6aa1d4049213aac1f4b6c3b6195cb3a
neelgeek <neelbhave91@gmail.com>
2020-04-20 02:21:31 -0400

    Added user page

--- brickdoor/src/main/java/com/example/brickdoor/controllers/AppController.java
+++ brickdoor/src/main/java/com/example/brickdoor/controllers/AppController.java
@@ -1,22 +1,32 @@
 package com.example.brickdoor.controllers;
 
+import com.example.brickdoor.daos.ReviewDao;
+import com.example.brickdoor.models.Review;
 import com.example.brickdoor.models.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
 @Controller
 public class AppController {
 
+  @Autowired
+  private ReviewDao reviewDao;
+
   @GetMapping("/")
   public ModelAndView indexRoute(HttpSession session) {
     User user = session.getAttribute("user") == null ? new User() : (User) session.getAttribute("user");
+    List<Review> reviewList = reviewDao.findAllReview();
     ModelAndView modelAndView = new ModelAndView("index");
     modelAndView.addObject("search", new SearchObject());
-    modelAndView.addObject("user",user);
+    modelAndView.addObject("reviewCount", reviewList.size());
+    modelAndView.addObject("user", user);
     return modelAndView;
   }
 
